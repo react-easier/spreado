2a1a819f7dfab80ce44de9472b6ef14aca060ad2
uno <licg9999@126.com>
2022-05-17 20:11:57 +0800

    test: remove unused

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/lib/'],
   coveragePathIgnorePatterns: ['/node_modules/', '/lib/'],
   setupFilesAfterEnv: ['<rootDir>/configs/setupAfterEnv.ts'],
 };
