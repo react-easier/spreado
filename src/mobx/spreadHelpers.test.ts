6c30473d925e4c1eb373655570a995812d21aa16
joky <jokycn0912@gmail.com>
2022-10-19 13:30:52 +0800

    refactor: rename internal functions of spread helpers

--- src/mobx/spreadHelpers.test.ts
+++ src/mobx/spreadHelpers.test.ts
@@ -4,7 +4,7 @@ import {noop, uniqueId} from 'lodash';
 import {reaction} from 'mobx';
 
 import {generateSpreadKey} from '../core';
-import {getSpreadIn, setSpreadOut, useSpreadIn, useSpreadOut} from './spreadHelpers';
+import {_getSpreadIn, _setSpreadOut, _useSpreadIn, _useSpreadOut} from './spreadHelpers';
 import {SpreadoMobXStore} from './SpreadoMobXStore';
 
 const store = new SpreadoMobXStore();
@@ -22,7 +22,7 @@ mocked(reaction).mockImplementation((expression, effect) => {
   return noop as never;
 });
 
-describe('useSpreadOut', () => {
+describe('_useSpreadOut', () => {
   test('returns proper value and updates mobx state', async () => {
     const counter: Record<string, number> = {};
     const index = uniqueId();
@@ -31,7 +31,7 @@ describe('useSpreadOut', () => {
     // returns input value immediately after initial call, before mobx state updated
     const inputValue1 = uniqueId();
     const {result, rerender, unmount} = renderHook(
-      (value) => useSpreadOut(store, counter, index, value),
+      (value) => _useSpreadOut(store, counter, index, value),
       {initialProps: inputValue1}
     );
     expect(result.current).toEqual(inputValue1);
@@ -68,11 +68,11 @@ describe('useSpreadOut', () => {
     const key = generateSpreadKey(index);
 
     const value1 = uniqueId();
-    const {unmount: unmount1} = renderHook(() => useSpreadOut(store, counter, index, value1));
+    const {unmount: unmount1} = renderHook(() => _useSpreadOut(store, counter, index, value1));
     expect(counter[key]).toEqual(1);
 
     const value2 = uniqueId();
-    const {unmount: unmount2} = renderHook(() => useSpreadOut(store, counter, index, value2));
+    const {unmount: unmount2} = renderHook(() => _useSpreadOut(store, counter, index, value2));
     expect(counter[key]).toEqual(2);
 
     unmount1();
@@ -82,13 +82,13 @@ describe('useSpreadOut', () => {
   });
 });
 
-describe('useSpreadIn', () => {
+describe('_useSpreadIn', () => {
   test('returns value in mobx state', () => {
     const index = uniqueId();
     const key = generateSpreadKey(index);
     const fallback = uniqueId();
     const value = uniqueId();
-    const {result} = renderHook(() => useSpreadIn(store, index, fallback));
+    const {result} = renderHook(() => _useSpreadIn(store, index, fallback));
     expect(result.current).toEqual(fallback);
     store.setState(key, value);
     act(triggerReactionOnce);
@@ -96,12 +96,12 @@ describe('useSpreadIn', () => {
   });
 });
 
-describe('setSpreadOut', () => {
+describe('_setSpreadOut', () => {
   test('updates mobx state directly if plain value given', () => {
     const index = uniqueId();
     const key = generateSpreadKey(index);
     const value = uniqueId();
-    const ret = setSpreadOut(store, index, value);
+    const ret = _setSpreadOut(store, index, value);
     expect(ret).toBe(value);
     expect(store.state[key]).toEqual(value);
   });
@@ -113,21 +113,21 @@ describe('setSpreadOut', () => {
     const callback = jest.fn(() => newValue);
     const oldValue = uniqueId();
     store.setState(key, oldValue);
-    const ret = setSpreadOut(store, index, callback);
+    const ret = _setSpreadOut(store, index, callback);
     expect(callback).toBeCalledWith(oldValue);
     expect(ret).toBe(newValue);
     expect(store.state[key]).toEqual(newValue);
   });
 });
 
-describe('getSpreadIn', () => {
+describe('_getSpreadIn', () => {
   test('returns value in mobx state', () => {
     const index = uniqueId();
     const key = generateSpreadKey(index);
     const fallback = uniqueId();
     const value = uniqueId();
     store.setState(key, value);
-    const ret = getSpreadIn(store, index, fallback);
+    const ret = _getSpreadIn(store, index, fallback);
     expect(ret).toEqual(value);
   });
 });
